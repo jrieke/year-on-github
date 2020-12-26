66e4a5d9c97382abf9c757d9935e13f2337e538f
Christoph Rieke <christoph.k.rieke@gmail.com>
2020-12-26 23:47:47 +0100

    outsource templates

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ import re
 import urllib
 
 import utils
+from tweet_templates import ORG_TEMPLATE, USER_TEMPLATE
 
 
 # Set up page.
@@ -81,37 +82,6 @@ def show_fineprint(runtime=None):
 show_fineprint()
 
 
-# Define templates for the tweet (separate for user and org). These will be filled
-# with the stats later and shown in the box.
-# TODO: Maybe refactor templating stuff to separate file.
-# TODO: Write updating stats in green.
-# TODO: Write URL to hottest repo here? Would be cool to offer sth to click on,
-#   but it always shows the link preview in the tweet.
-# TODO: Encoding the 🧑‍💻 emoji in a link works in the browser but not on Android. Problem
-#   is probably the Twitter app, see if there's a workaround or replace emoji.
-user_template = """
-<p id="tweet">
-My year on Github 2020 🧑‍💻✨ {username}
-<br><br>
-📬 Commits/Issues/PRs: {contributions}<br>
-🏝️ Repos contributed to: {repos_contributed_to}<br>
-⭐ New stars: {new_stars}<br>
-🔥 Hottest repo: {hottest_repo}
-<br><br>
-Share your stats: <a href="https://gh2020.jrieke.com">gh2020.jrieke.com</a> | Built by <a href="https://twitter.com/jrieke">@jrieke</a> w/ <a href="https://twitter.com/streamlit">@streamlit</a> <a href="https://twitter.com/github">@github</a> | <a href="https://twitter.com/search?q=%23github2020">#github2020</a>
-</p>
-"""
-
-org_template = """
-<p id="tweet">
-Our year on Github 2020 🧑‍💻✨ {username}
-<br><br>
-⭐ New stars: {new_stars}<br>
-🔥 Hottest repo: {hottest_repo}
-<br><br>
-Share your stats: <a href="https://gh2020.jrieke.com">gh2020.jrieke.com</a> | Built by <a href="https://twitter.com/jrieke">@jrieke</a> w/ <a href="https://twitter.com/streamlit">@streamlit</a> <a href="https://twitter.com/github">@github</a> | <a href="https://twitter.com/search?q=%23github2020">#github2020</a>
-</p>
-"""
 
 
 def show_tweet(stats):
