d60fb795072e24c21e21c0a3646a477583e47abe
Christoph Rieke <christoph.k.rieke@gmail.com>
2020-12-27 01:44:11 +0100

    add background color to template

--- app/tweet_templates.py
+++ app/tweet_templates.py
@@ -12,7 +12,7 @@ My year on Github 2020 🧑‍💻✨ {username}
 <br><br>
 📬 Commits/Issues/PRs: {contributions}<br>
 🏝️ Repos contributed to: {repos_contributed_to}<br>
-⭐ New stars: {new_stars}<br>
+⭐ New stars: <span style='background-color: #FFFFFF'>{new_stars}</span><br>
 🔥 Hottest repo: {hottest_repo}
 <br><br>
 Share your stats: <a href="https://gh2020.jrieke.com">gh2020.jrieke.com</a> | Built by <a href="https://twitter.com/jrieke">@jrieke</a> w/ <a href="https://twitter.com/streamlit">@streamlit</a> <a href="https://twitter.com/github">@github</a> | <a href="https://twitter.com/search?q=%23github2020">#github2020</a>
@@ -23,7 +23,7 @@ ORG_TEMPLATE = """
 <p id="tweet">
 Our year on Github 2020 🧑‍💻✨ {username}
 <br><br>
-⭐ New stars: {new_stars}<br>
+⭐ New stars: <span style='background-color: #FFFFFF'>{new_stars}</span><br>
 🔥 Hottest repo: {hottest_repo}
 <br><br>
 Share your stats: <a href="https://gh2020.jrieke.com">gh2020.jrieke.com</a> | Built by <a href="https://twitter.com/jrieke">@jrieke</a> w/ <a href="https://twitter.com/streamlit">@streamlit</a> <a href="https://twitter.com/github">@github</a> | <a href="https://twitter.com/search?q=%23github2020">#github2020</a>
