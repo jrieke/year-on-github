e1150281ef793b9a1b5b83250dd315ded041de9b
Christoph Rieke <christoph.k.rieke@gmail.com>
2020-12-27 02:06:53 +0100

    rmv not required from v1

--- app/main.py
+++ app/main.py
@@ -104,12 +104,6 @@ if username or (clicked and username):
                 unsafe_allow_html=True,
             )
 
-        # Overwrite one last time to end colorize update.
-        tweet_box.write(
-            tweet_html.replace("#32CD32", "#FFFFFF"),
-            unsafe_allow_html=True,
-        )
-
         tweet_button_html = content.construct_tweet_button(tweet_html)
         tweet_button.write(
             tweet_button_html,
