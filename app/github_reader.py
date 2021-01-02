e0bf393b695f2648c9a80fbec00ae31d31260f62
Johannes Rieke <johannes.rieke@gmail.com>
2021-01-02 02:29:46 +0100

    Get number of contributors for repos

--- app/github_reader.py
+++ app/github_reader.py
@@ -102,6 +102,8 @@ def _query_user(username: str, year: int) -> Tuple:
     # TODO: Maybe do this with the GraphQL API. Bit more complicated to handle
     #   pagination though + it's a lot of new code for 0.5 s performance increase.
     own_repo_stars = {}
+    contributor_names = set()  # only used if is_org is True
+    contributors_greater_than = False
     endpoint = api.repos.list_for_org if is_org else api.repos.list_for_user
     num_pages = 1 + int(num_repos / 100)
     for repo in pages(endpoint, num_pages, username).concat():
@@ -121,13 +123,43 @@ def _query_user(username: str, year: int) -> Tuple:
             new_stars = None
             print("Needs intense analysis, do later")
         own_repo_stars[repo.full_name] = new_stars
+
+        # For orgs: Count number of contributors across all repos.
+        # TODO: For large orgs, this can take a significant amount of time. Should show
+        #   progress in more detail here.
+        year_start = datetime(year, 1, 1, 0, 0, 0)
+        year_end = datetime(year, 12, 31, 23, 59, 59)
+        repo_contributor_names = set()
+        # TODO: This only returns the 100 most active contributors and there's no
+        #   way to get more, i.e. it doesn't work for very popular repos. Maybe look for 
+        #   another way to do this.
+        contributors = api.repos.get_contributors_stats(repo.owner.login, repo.name)
+        if len(contributors) == 100:
+            contributors_greater_than = True
+        for contributor in contributors:
+            # print(
+            #     f"Analyzing contributor f{contributor.author.login} ({contributor.total} contributions)"
+            # )
+            for week_stats in contributor.weeks:
+                week = datetime.fromtimestamp(int(week_stats.w))
+                if week_stats.c > 0 and week > year_start and week < year_end:
+                    # print(f"Found {week_stats.c} contributions in week {week}")
+                    # print()
+                    repo_contributor_names.add(contributor.author.login)
+                    break
+        print(f"Found {len(repo_contributor_names)} contributors")
+        contributor_names |= repo_contributor_names
+        
         print()
 
     # 3) Query GraphQL API to get contribution counts + external repos.
     if is_org:
         # TODO: Maybe count commits (+ maybe issues/prs) for this year across all repos.
         contributions = 0
-        repos_contributed_to = 0
+        # TODO: Temporarily storing this in `repos_contributed_to`, rename if I keep it.
+        repos_contributed_to = len(contributor_names)
+        if contributors_greater_than:
+            repos_contributed_to = ">" + str(repos_contributed_to)
         external_repo_stars = {}
     else:
         url = "https://api.github.com/graphql"
